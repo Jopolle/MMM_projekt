276c63a74adbb9b83ec7e74140ee4dac2f189a8f
Jopolle <46864640+Jopolle@users.noreply.github.com>
2024-03-29 16:21:30 +0100

    Add project files.

---
+++ MMM_projekt_matplot_test.cpp
@@ -0,0 +1,20 @@
+﻿// MMM_projekt_matplot_test.cpp : Defines the entry point for the application.
+//
+
+#include "MMM_projekt_matplot_test.h"
+using namespace matplot;
+
+int main()
+{
+    //auto x = linspace(-4, 4);
+    //auto y = transform(x, [](double x) { return pow(x, 3) - 12 * x; });
+    //plot(x, y);
+
+    auto e = ellipse(-3.5, 11, 3.5, 8);
+    auto r = rectangle(-3.5, 11, 3.5, 8);
+    r->color("red");
+
+    show();
+
+	return 0;
+}
---
+++ MMM_projekt_matplot_test.h
@@ -0,0 +1,10 @@
+﻿// MMM_projekt_matplot_test.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <matplot/matplot.h>
+#include <cmath>
+
+// TODO: Reference additional headers your program requires here.
